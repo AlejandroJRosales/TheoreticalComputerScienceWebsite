9548a2d74d21d49f0c4b506bf77d0b935916b823
Alejandro Rosales <alejandrorosales479@gmail.com>
2020-11-10 08:17:18 +0000

    Shadow for table of contents of button

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
   $("#footer-placeholder").load("footer.html");
 });
 
+$(document).ready(function(){
+  $("#collapsible-table-of-contents-placeholder").load("collapsiblecontents.html");
+});
+
 window.MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']]

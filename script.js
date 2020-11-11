e190d8eba542ac20cca56c8b3b289ceaf482f9a8
Alejandro Rosales <alejandrorosales479@gmail.com>
2020-11-11 08:49:37 +0000

    ids to classes

--- script.js
+++ script.js
@@ -19,11 +19,13 @@ $(document).ready(function(){
     $("h1").css({"font-size": "70px"});
     // $("h5").css({"font-size": "40px"});
     $("p").css({"font-size": "30px"});
-    $("#psuedo-code").css({"font-size": "17px"});
-    $("#collapsible-contents-button").css({"top": "10%", "right": "2%"});
-    $("#category-header").css({"font-size": "50px"});
-    $("#section-header").css({"font-size": "45px"});
-    // $("#math-notation").css({"font-size": "35px"});
+    $(".psuedo-code").css({"font-size": "17px"});
+    $(".collapsible-contents-button").css({"top": "10%", "right": "2%"});
+    $(".category-header").css({"font-size": "300%"});
+    $(".section-header").css({"font-size": "230%"});
+    $(".subsection-header").css({"font-size": "190%"});
+    // $(".math-notation").css({"font-size": "120%"});
+    $("#collapse").css({"overflow": "scroll"});
   }
 });
 
@@ -35,15 +37,11 @@ $(document).ready(function(){
 // });
 
 $(document).ready(function(){
-  $("#nav-placeholder").load("nav.html");
+  $(".nav-placeholder").load("nav.html");
 });
 
 $(document).ready(function(){
-  $("#footer-placeholder").load("footer.html");
-});
-
-$(document).ready(function(){
-  $("#collapsible-table-of-contents-placeholder").load("collapsiblecontents.html");
+  $(".footer-placeholder").load("footer.html");
 });
 
 window.MathJax = {
